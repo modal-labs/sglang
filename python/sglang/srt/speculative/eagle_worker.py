0244292fbcf3a4103febebb807b76e13218b4622
Timothy Feng <timf01089@gmail.com>
2025-08-20 18:56:13 -0400

    avoid unnecessary backup

--- python/sglang/srt/speculative/eagle_worker.py
+++ python/sglang/srt/speculative/eagle_worker.py
@@ -682,11 +682,6 @@ class EAGLEWorker(TpModelWorker):
     def forward_draft_extend_after_decode(self, batch: ModelWorkerBatch):
         assert isinstance(batch.spec_info, EagleDraftInput)
         # Backup fields that will be modified in-place
-        seq_lens_backup = batch.seq_lens.clone()
-        req_pool_indices_backup = batch.req_pool_indices
-        accept_length_backup = batch.spec_info.accept_length
-        return_logprob_backup = batch.return_logprob
-
         input_is_idle = batch.forward_mode.is_idle()
 
         if not input_is_idle and batch.spec_info.verified_id.numel() == 0:
@@ -759,15 +754,9 @@ class EAGLEWorker(TpModelWorker):
 
         self._detect_nan_if_needed(logits_output)
 
-        # Restore backup.
-        # This is because `seq_lens` can be modified in `prepare_extend_after_decode`
         batch.forward_mode = (
             ForwardMode.DECODE if not input_is_idle else ForwardMode.IDLE
         )
-        batch.seq_lens = seq_lens_backup
-        batch.req_pool_indices = req_pool_indices_backup
-        batch.spec_info.accept_length = accept_length_backup
-        batch.return_logprob = return_logprob_backup
 
     def capture_for_decode(
         self, logits_output: LogitsProcessorOutput, draft_input: EagleDraftInput
